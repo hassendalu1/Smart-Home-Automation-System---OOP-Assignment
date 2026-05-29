4245b5b65a46fab53959c8fb1ffa77374eb42942
Eyuelzelalem <70477341+Eyuelzelalem@users.noreply.github.com>
2026-05-29 11:34:04 +0300

    edited by eyuel
    Removed TemperatureSensor class and related comments.

--- src/SmartHome.java
+++ src/SmartHome.java
@@ -1,29 +1,4 @@
-[5/17/2026 6:09 PM] Hsn: get to google and search https://github.com/hassendalu1/Simple-bank-system..git
-[5/28/2026 11:34 AM] Hsn: public class TemperatureSensor extends Device {
-
-    private double temperature;
-
-    public TemperatureSensor(String deviceName) {
-        super(deviceName);
-        temperature = 22.5;
-    }
-
-    @Override
-    public void turnOn() {
-        setStatus(true);
-        System.out.println(getDeviceName() +
-                " sensor activated");
-    }
-
-    @Override
-    public void displayStatus() {
-        System.out.println("Temperature Sensor -> " +
-                getDeviceName() +
-                " : " +
-                temperature + "°C");
-    }
-}
-[5/28/2026 11:35 AM] Hsn: import java.util.ArrayList;
+import java.util.ArrayList;
 
 public class SmartHome {
 
